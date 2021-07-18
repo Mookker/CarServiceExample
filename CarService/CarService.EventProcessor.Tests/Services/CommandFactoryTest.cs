c48b04934a76609cd97a8d65c7373c5a135ffff7
ejenaka <interpaint1975@gmail.com>
2021-07-18 15:23:21 +0300

    added Event Processor tests

---
+++ CarService/CarService.EventProcessor.Tests/Services/CommandFactoryTest.cs
@@ -0,0 +1,93 @@
+﻿using CarService.AppCore.Models.EventModels;
+using CarService.AppCore.Models.Events;
+using CarService.EventProcessor.Cqrs.Commands;
+using CarService.EventProcessor.Interfaces;
+using CarService.EventProcessor.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CarService.EventProcessor.Tests.Services
+{
+    public class CommandFactoryTest
+    {
+        private readonly ICommandFactory _factory;
+
+        public CommandFactoryTest()
+        {
+            _factory = new CommandFactory();
+        }
+
+        [Fact]
+        public void Factory_Should_Return_CreateRepairOrderCommand()
+        {
+            // Arrange
+            string jsonData = @"{
+                                 'Type': 'RepairOrderCreatedEvent', 
+                                 'Data': {
+                                     'id' : 'ac307cca-0430-49b4-bbbf-dc59c8cbb177',
+                                     'price' : 100,
+                                     'orderDate' : '2021-06-17T00:00:00.000Z',
+                                     'carId' : '35992974-21ea-4f61-b715-2dfaed663b73'
+                                  }
+                                }";
+
+            var @event = JsonConvert.DeserializeObject<BaseEvent<JObject>>(jsonData);
+
+            // Act
+            var command = _factory.CreateCommand(@event);
+
+            // Assert
+            Assert.NotNull(command);
+            Assert.IsAssignableFrom<CreateRepairOrderCommand>(command);
+        }
+
+        [Fact]
+        public void Factory_Should_Return_DeleteRepairOrderCommand()
+        {
+            // Arrange
+            string jsonData = @"{
+                                 'Type': 'RepairOrderDeletedEvent', 
+                                 'Data': {'id' : 'ac307cca-0430-49b4-bbbf-dc59c8cbb177'}
+                                }";
+
+            var @event = JsonConvert.DeserializeObject<BaseEvent<JObject>>(jsonData);
+
+            // Act
+            var command = _factory.CreateCommand(@event);
+
+            // Assert
+            Assert.NotNull(command);
+            Assert.IsAssignableFrom<DeleteRepairOrderCommand>(command);
+        }
+
+        [Fact]
+        public void Factory_Should_Return_UpdateRepairOrderCommand()
+        {
+            // Arrange
+            string jsonData = @"{
+                                 'Type': 'RepairOrderUpdatedEvent', 
+                                 'Data': {
+                                     'id' : 'ac307cca-0430-49b4-bbbf-dc59c8cbb177',
+                                     'price' : 100,
+                                     'orderDate' : '2021-06-17T00:00:00.000Z',
+                                     'carId' : '35992974-21ea-4f61-b715-2dfaed663b73'
+                                  }
+                                }";
+
+            var @event = JsonConvert.DeserializeObject<BaseEvent<JObject>>(jsonData);
+
+            // Act
+            var command = _factory.CreateCommand(@event);
+
+            // Assert
+            Assert.NotNull(command);
+            Assert.IsAssignableFrom<UpdateRepairOrderCommand>(command);
+        }
+    }
+}
---
+++ CarService/CarService.EventProcessor.Tests/Services/RepairOrdersRedisEventListenerTest.cs
@@ -0,0 +1,66 @@
+﻿using CarService.EventProcessor.Interfaces;
+using CarService.EventProcessor.Services;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using StackExchange.Redis;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CarService.EventProcessor.Tests.Services
+{
+    public class RepairOrdersRedisEventListenerTest
+    {
+        private readonly Mock<IMediator> _mediator;
+        private readonly ICommandFactory _commandFactory;
+        private readonly IConnectionMultiplexer _connection;
+        private readonly ISubscriber _subscriber;
+        private readonly ChannelMessageQueue _channel;
+        private readonly IConfiguration _configuration;
+        private readonly RepairOrdersRedisEventListener _eventListener;
+
+        private const string CONNECTION_STRING = "localhost:6379";
+        private const string CHANNEL_NAME = "repairOrders";
+        private const string TEST_MESSAGE = "test message";
+
+        public RepairOrdersRedisEventListenerTest()
+        {
+            _mediator = new Mock<IMediator>();
+            _commandFactory = new CommandFactory();
+            _connection = ConnectionMultiplexer.Connect(CONNECTION_STRING);
+            _subscriber = _connection.GetSubscriber();
+            _channel = _subscriber.Subscribe(CHANNEL_NAME);
+            _configuration = BuildConfigurationForEventListener();
+            _eventListener = new RepairOrdersRedisEventListener(_configuration, _mediator.Object, _commandFactory);
+        }
+
+        private IConfiguration BuildConfigurationForEventListener()
+        {
+            var settings = @"{""ConnectionStrings"" : {""Redis"" : ""localhost:6379""}}";
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(settings)));
+
+            return builder.Build();
+        }
+
+        [Fact]
+        public void RedisConnection_Success()
+        {
+            Assert.True(_connection.IsConnected);
+        }
+
+        [Fact]
+        public async Task EventListener_OnMessage_Success()
+        {
+            // Act
+            await _subscriber.PublishAsync(CHANNEL_NAME, TEST_MESSAGE);
+            var message = await _channel.ReadAsync();
+
+            // Assert
+            Assert.True(message.Message.HasValue);
+            Assert.Equal(TEST_MESSAGE, message.Message.ToString());
+        }
+    }
+}
